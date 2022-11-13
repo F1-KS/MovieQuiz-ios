c0a90e51cb504bf576ddd49acb175e38ab780d6b
F1-KS <f1-ks@mail.ru>
2022-11-13 20:17:14 +0300

    123

--- MovieQuiz/Resources/extUIColor.swift
+++ MovieQuiz/Resources/extUIColor.swift
@@ -1,7 +1,12 @@
+//
+//  extUIColor.swift
+//  MovieQuiz
+//
+//  Created by Dmitry Ivanov on 12.11.2022.
+//
+
 import Foundation
 import UIKit
-
-//extension UIColor { }
 
 extension UIColor {
     static var ypBlack: UIColor { UIColor (named: "ypBlack")! }
